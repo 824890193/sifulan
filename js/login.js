f067c93a6063ed9d4a40dc30acc28b776de0b5c3
lipei <824890193@qq.com>
2018-10-16 22:16:48 +0800

    登录 注册基本完成

--- js/login.js
+++ js/login.js
@@ -1,58 +1,92 @@
+//点击特效
+$("input").click(function(){
+    $(this).parent().css("border-bottom","1px solid #000");
+    $(this).next().css("display","block");
+})
+$("input").blur(function(){
+    $("input").parent().css("border-bottom","1px solid #ccc");
+    $("input").next().css("display","none");    
+}) 
+//hover特效
+$(".p1").hover(function(){
+    $(this).children(".img1").css("display","none");
+    $(this).children(".img2").css("display","block");
+},function(){
+    $(this).children(".img1").css("display","block");
+    $(this).children(".img2").css("display","none");
+})
+
 //表单验证
 //1. 用户名;
 //验证手机号码
-var oMbp = document.getElementById("mbp");       
+var oMbp = document.getElementById("mbp");  
+var oBorder1 = document.getElementById("name");
+var pass_1 = document.getElementById("pass1");
+var not1 = document.getElementById("name").children;     
 oMbp.onblur = function(){
     //只能纯数字11位;
-    var reg4 = /^[0-9]{11}$/;
+    var reg_1 = /^[0-9]{11}$/;
     var sMbp = oMbp.value;
-    if(reg4.test(sMbp)){
-        oMbp.style.borderColor = "green";
+    if(reg_1.test(sMbp)){
+        not1[3].style.display = "none"; 
+        pass_1.style.display = "none"; 
+        oMbp.style.borderBottomColor = "green";
 
+        //验证密码
+        var oPsw = document.getElementById("psw");
+        var oBorder2 = document.getElementById("secret");
+        var pass_2 = document.getElementById("pass2");
+        var not2 = document.getElementById("secret").children;
+        oPsw.onblur = function(){
+            var sPsw = oPsw.value;
+            var reg_3 = /^[0-9a-zA-Z\!\@\#\$\%\^\&\*\)\_]{6,16}$/;
+            if(reg_3.test(sPsw)){ 
+                not2[3].style.display = "none"; 
+                pass_2.style.display = "none"; 
+                oBorder2.style.borderBottomColor = "green";        
+                //填写成功点击按钮
+                var btn_1 = document.getElementById("btn1");            
+                btn_1.style.display = "block";           
+            }else{ 
+                not2[3].style.display = "block";
+                pass_2.style.display = "block"; 
+                oBorder2.style.borderBottomColor = "red";
+            }    
+        }
     }else{
-        oMbp.style.borderColor = "red";
+        //验证邮箱       
+        //只能以数字字母开头 数字字母下划线 6 ~ 20位;
+        var reg_2 = /^[0-9a-z][0-9a-z_]{5,19}@[0-9a-z]{2,6}\.[a-z]{2,5}$/;
+        if(reg_2.test(sMbp)){
+            not1[3].style.display = "none"; 
+            pass_1.style.display = "none";
+            oBorder1.style.borderBottomColor = "green"; 
+            //验证密码
+            var oPsw = document.getElementById("psw");
+            var oBorder2 = document.getElementById("secret");
+            var pass_2 = document.getElementById("pass2");
+            var not2 = document.getElementById("secret").children;
+            oPsw.onblur = function(){
+                var sPsw = oPsw.value;
+                var reg_3 = /^[0-9a-zA-Z\!\@\#\$\%\^\&\*\)\_]{6,16}$/;
+                if(reg_3.test(sPsw)){ 
+                    not2[3].style.display = "none"; 
+                    pass_2.style.display = "none"; 
+                    oBorder2.style.borderBottomColor = "green";        
+                    //填写成功点击按钮
+                    var btn_1 = document.getElementById("btn1");            
+                    btn_1.style.display = "block";           
+                }else{ 
+                    not2[3].style.display = "block";
+                    pass_2.style.display = "block"; 
+                    oBorder2.style.borderBottomColor = "red";
+                }    
+            }              
+        }else{
+            not1[3].style.display = "block";
+            pass_1.style.display = "block"; 
+            oBorder1.style.borderBottomColor = "red";
+        }       
     }
 };
-//验证邮箱
-var oEml = document.getElementById("eml");       
-oEml.onblur = function(){
-    //只能以数字字母开头 数字字母下划线 6 ~ 20位;
-    var reg3 = /^[0-9a-z][0-9a-z_]{5,19}@[0-9a-z]{2,6}\.[a-z]{2,5}$/;
-    var sEml = oEml.value;
-    if(reg3.test(sEml)){
-        oEml.style.borderColor = "green";
 
-    }else{
-        oEml.style.borderColor = "red";
-    }
-};
-//验证密码
-var oPwd = document.getElementById("pwd");
-oPwd.onblur = function(){
-    var sPwd = oPwd.value;
-    var rate = 0;
-    var regNumber = /\d/;
-    var regWord = /[a-zA-Z]/;
-    var regSymbol = /[\!\@\#\$\%\^\&\*\)\_]/;
-    //密码安全级别评定
-    if(regNumber.test(sPwd)){
-        rate++
-    }
-    if(regWord.test(sPwd)){
-        rate++
-    }
-    if(regSymbol.test(sPwd)){
-        rate++
-    }
-    switch(rate){
-        case 1 : 
-        oPwd.style.borderColor = "red";
-        break;
-        case 2 : 
-        oPwd.style.borderColor = "yellow";
-        break;
-        case 3 : 
-        oPwd.style.borderColor = "green";
-        break;
-    }
-}
