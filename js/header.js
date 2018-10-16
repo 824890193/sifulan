59119007b4d5428379a447ca28f8967af944c3bb
lipei <824890193@qq.com>
2018-10-16 23:14:54 +0800

    placeholder轮播完成

--- js/header.js
+++ js/header.js
@@ -11,7 +11,7 @@ timer= function(){
         setTimeout(function(){
             oUl.className = "move";
             oUl.style.top = -30 * nowIndex + "px";
-        },50)
+        },500)
         return 0;
     }else{
         nowIndex ++;           
@@ -28,4 +28,29 @@ oUl.onmouseleave = function(){
     autoPlayTimer = setInterval(timer, 3000);
 }
 var autoPlayTimer = setInterval(timer, 3000);
+// 搜索框轮播条
+var content = document.getElementById("search");
+var letter = document.querySelectorAll("#innersearch a");
+var p = 0;
+function innerauto(){
+    if(p>=letter.length-1){
+        p = 0;
+    }else{
+        p++;
+    }
+    auto();
+}
 
+function auto(){
+    content.setAttribute("placeholder",letter[p].innerHTML);    
+}
+content.onfocus = function(){
+    clearInterval(autoTimer);
+    content.setAttribute("placeholder","");
+}
+content.onblur = function(){
+    clearInterval(autoTimer);
+    content.setAttribute("placeholder","请输入您要搜索的关键词");
+    autoTimer = setInterval(innerauto,2500);
+}
+var autoTimer = setInterval(innerauto,2500) 
